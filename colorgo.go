92e595eed05dad96a6929dd76c6b35ee106953f4
Lornzo <a6288678@hotmail.com>
2021-02-17 14:04:54 +0800

    update

--- colorgo.go
+++ colorgo.go
@@ -1,1 +1,24 @@
 package colorgo
+
+import (
+	"colorgo/colors"
+	"fmt"
+)
+
+type colorgo struct {
+	selectedColor string
+}
+
+func NewColorGo() *colorgo {
+	result := new(colorgo)
+	result.SetColor(colors.DebugColor)
+	return result
+}
+
+func (c *colorgo) SetColor(color string) {
+	c.selectedColor = color
+}
+
+func (c *colorgo) Println(str string) {
+	fmt.Println(fmt.Printf(c.selectedColor, str))
+}
---
+++ colors/colors.go
@@ -0,0 +1,11 @@
+package colors
+
+const (
+	//預設顏色：白色
+	DefaultColor = "\033[1;34m%s\033[0m"
+	InfoColor    = "\033[1;34m%s\033[0m"
+	NoticeColor  = "\033[1;36m%s\033[0m"
+	WarningColor = "\033[1;33m%s\033[0m"
+	ErrorColor   = "\033[1;31m%s\033[0m"
+	DebugColor   = "\033[0;36m%s\033[0m"
+)
